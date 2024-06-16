dcbf21887c5ee92eaab4b17ccc36bff60abb8403
Default Box <134133080+thedefaultbox@users.noreply.github.com>
2024-06-16 19:19:55 +1000

    fix build script again

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 echo Building for Linux...
 set GOOS=linux
 set GOARCH=amd64
-go build -o /Linux/bulk-dl.exe
+go build -o ./Linux/bulk-dl
 if %errorlevel% neq 0 (
     echo Failed to build for Linux
     exit /b 1
@@ -12,7 +12,7 @@ if %errorlevel% neq 0 (
 echo Building for Windows...
 set GOOS=windows
 set GOARCH=amd64
-go build -o /Windows/bulk-dl.exe
+go build -o ./Windows/bulk-dl.exe
 if %errorlevel% neq 0 (
     echo Failed to build for Windows
     exit /b 1
