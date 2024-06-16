ac42af67ef4ae1d2a2eb4a5fb28516b71be8e35e
Default Box <134133080+thedefaultbox@users.noreply.github.com>
2024-06-16 19:16:28 +1000

    fix building script and get rid of mac because fuck apple

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 echo Building for Linux...
 set GOOS=linux
 set GOARCH=amd64
-go build -o myprogram-linux
+go build -o /Linux/bulk-dl.exe
 if %errorlevel% neq 0 (
     echo Failed to build for Linux
     exit /b 1
@@ -12,27 +12,9 @@ if %errorlevel% neq 0 (
 echo Building for Windows...
 set GOOS=windows
 set GOARCH=amd64
-go build -o myprogram-windows.exe
+go build -o /Windows/bulk-dl.exe
 if %errorlevel% neq 0 (
     echo Failed to build for Windows
-    exit /b 1
-)
-
-echo Building for macOS (Intel)...
-set GOOS=darwin
-set GOARCH=amd64
-go build -o myprogram-macos
-if %errorlevel% neq 0 (
-    echo Failed to build for macOS (Intel)
-    exit /b 1
-)
-
-echo Building for macOS (ARM)...
-set GOOS=darwin
-set GOARCH=arm64
-go build -o myprogram-macos-arm64
-if %errorlevel% neq 0 (
-    echo Failed to build for macOS (ARM)
     exit /b 1
 )
 
